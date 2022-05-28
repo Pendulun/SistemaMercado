b7154e52ea299eec481f61dd2f1be1f60191ebc9
Letícia Alves <48053042+leticiasma@users.noreply.github.com>
2022-05-28 18:40:03 -0300

    feat: Cadastro Produto
    Página de ver Produtos e página de cadastro de Produto

--- comercio/products/views.py
+++ comercio/products/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from json import load
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import Product
+from django.urls import reverse
+from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    template = loader.get_template("products/index.html")
+    myproducts = Product.objects.all().values()#[:3]
+
+    context = {
+        "myproducts": myproducts,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+    template = loader.get_template("products/addProduct.html")
+    return HttpResponse(template.render({}, request))
+
+def saveproduct(request):
+    nome = request.POST['name']
+    preco = request.POST['price']
+    marca = request.POST['brand']
+    estoque = 0
+    vendidos = 0
+    try:
+        novoProduto = Product(name=nome, price=preco, brand=marca, stock=estoque, sold=vendidos)
+        novoProduto.save()
+    except:
+        messages.error(request, "Complete corretamente todos os campos de cadastro!")
+        return HttpResponseRedirect(reverse("products:productregistry"))
+    else:
+        return HttpResponseRedirect(reverse("products:index"))
