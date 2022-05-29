d3a5c2b1ed4c50e7079e0ec264eb7aebbbceca04
Daniel Campos <danielcampos@dcc.ufmg.br>
2022-05-29 00:00:29 -0300

    Index e Cadastro de Fornecedor

--- comercio/suppliers/models.py
+++ comercio/suppliers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    telephone = models.CharField(max_length=255)
+    cnpj = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    #product list
+
+    def __str__(self):
+        return self.name+""+self.cnpj
--- comercio/suppliers/views.py
+++ comercio/suppliers/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from .models import Supplier
+from django.contrib import messages
+
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'suppliers/index.html'
+    context_object_name = "suppliers_list"
+
+    def get_queryset(request):
+        return Supplier.objects.all()
+
+
+class CadastroFornecedorView(generic.CreateView):
+    template_name_suffix = '_create_form'
+    model = Supplier
+    fields = ['name','telephone','cnpj','address']
+
+def savesupplier(request):
+    nome = request.POST['name']
+    phone = request.POST['telephone']
+    cnpj = request.POST['cnpj']
+    endereco = request.POST['address']
+    try:
+        novoSupplier = Supplier(name=nome, telephone=phone, cnpj=cnpj, address=endereco)
+        novoSupplier.save()
+    except:
+        messages.error(request, "Complete corretamente todos os campos de cadastro!")
+        return HttpResponseRedirect(reverse("suppliers:supplierregistry"))
+    else:
+        return HttpResponseRedirect(reverse("suppliers:index"))
