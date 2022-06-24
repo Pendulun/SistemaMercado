b8a2b6d28e711f563e3305301a6bbc73679cb6c9
Daniel Campos <danielcampos@dcc.ufmg.br>
2022-06-24 13:24:46 -0300

    refactor: Melhorando nomes testes products

--- comercio/products/tests.py
+++ comercio/products/tests.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 # Create your tests here.
 class ProductIndexViewTests(TestCase):
-    def test_no_products(self):
+    def test_index_no_products_warning_message(self):
         """
         Testa se aparece uma mensagem de erro apropriada quando não houverem produtos cadastrados
         """
@@ -13,7 +13,7 @@ class ProductIndexViewTests(TestCase):
         self.assertContains(response, "Não existem produtos cadastrados.")
         self.assertQuerysetEqual(response.context['myproducts'], [])
 
-    def test_add_one_product(self):
+    def test_new_product_sent_to_index(self):
         """
         Testa se o produto recém cadastrado é enviado para a página de index
         """
@@ -43,7 +43,7 @@ class ProductIndexViewTests(TestCase):
         novoproduto.delete()
 
 class ProductRegisterViewTests(TestCase):
-    def test_register_product(self):
+    def test_registered_product_sent_to_index(self):
         """
         Testa se o produto cadastrado na tela de cadastro é enviado para o index
         """
@@ -71,7 +71,7 @@ class ProductRegisterViewTests(TestCase):
 
         self.assertEqual(response.context['myproducts'][0], prodDict)
     
-    def test_register_product_with_no_info(self):
+    def test_cant_register_product_invalid_info(self):
         """
         Testa se uma mensagem de erro aparece caso seja feito um cadastro com valores inválidos
         """
